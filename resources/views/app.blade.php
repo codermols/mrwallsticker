04e7c734b1c01b0664519aac00c71966c1e9e5c6
codermols@gmail.com <Mp6par0gods>
2016-11-27 16:19:20 +0100

    små ændringer til forside-template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,12 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>MrWallsticker | Design din egen wallsticker, canvas og tapet</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
 	<h1>MrWallsticker</h1>
 	@yield('content')
+
+	<script src="//code.juery.com/jquery-1.11.3.min.js"></script>
 </body>
 </html>
