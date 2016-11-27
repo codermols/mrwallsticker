ce21151d89b53969b128e77aa6dcd7145a4573fb
codermols@gmail.com <Mp6par0gods>
2016-11-27 16:13:53 +0100

    nyt view til forside. Kun en tester

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>MrWallsticker | Design din egen wallsticker, canvas og tapet</title>
+</head>
+<body>
+	<h1>MrWallsticker</h1>
+	@yield('content')
+</body>
+</html>
